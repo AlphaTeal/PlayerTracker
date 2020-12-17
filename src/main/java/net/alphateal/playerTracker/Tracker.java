71f126d2e8f26fda5fdc8d513686d6b540a34b5e
Joey Gore <joey@joeygore.dev>
2020-12-16 19:17:00 -0500

    Untested, Minimal Viable Product

--- src/main/java/net/alphateal/playerTracker/Tracker.java
+++ src/main/java/net/alphateal/playerTracker/Tracker.java
@@ -3,13 +3,21 @@ package net.alphateal.playerTracker;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.Command;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.CompassMeta;
+import org.bukkit.material.MaterialData;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.data.BlockData;
 
 public class Tracker implements CommandExecutor {
 
@@ -34,11 +42,6 @@ public class Tracker implements CommandExecutor {
             Player hunter = (Player)sender;
             Player huntee = Bukkit.getPlayer(args[1]);
 
-            // can setCompassTarget and getCompassTarget
-            // But I don't think it lets you have more than one
-
-            // Might need to add something to the scheduler too
-
             // AHA! Lodestones are the key!
             // Basically, each compass spawned will behave like a lodestone
             // compass with the huntee as the lodestone
@@ -48,19 +51,11 @@ public class Tracker implements CommandExecutor {
                 return false;
             }
 
-            Location hunteeLocation = huntee.getLocation();
-            hunter.setCompassTarget(hunteeLocation);
-
-            // /tracker create DjBrownie
-            // Should spawn a new compass that always points to DjBrownie
-            
-            ItemStack tracker = new ItemStack(Material.COMPASS);
-            tracker.setAmount(1);
+            ItemStack tracker = new ItemStack(Material.COMPASS, 1);
             CompassMeta trackerMeta = (CompassMeta)tracker.getItemMeta();
             trackerMeta.setDisplayName(huntee.getDisplayName() + " Tracker");
-            trackerMeta.setLodestoneTracked(true);
-            trackerMeta.setLodestone(hunteeLocation); //Might need to schedule
-
+            trackerMeta.setLodestoneTracked(false);
+            trackerMeta.setLodestone(huntee.getLocation());
             tracker.setItemMeta(trackerMeta);
 
             hunter.getInventory().addItem(tracker);
@@ -71,14 +66,23 @@ public class Tracker implements CommandExecutor {
             Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
                 @Override
                 public void run() {
-                    sender.sendMessage("Refreshing " + huntee.getDisplayName() + " tracker...");
-                    Location hunteeLocation = huntee.getLocation();
-                    trackerMeta.setLodestoneTracked(true);
-                    trackerMeta.setLodestone(hunteeLocation);
-                    trackerMeta.setDisplayName("DEBUG");
-                    tracker.setItemMeta(trackerMeta);
+                    PlayerInventory hunterInv = hunter.getInventory();
+                    HashMap<Integer, ? extends ItemStack> allCompii = hunterInv.all(Material.COMPASS);
+
+                    for (int i = 0; i < allCompii.size(); i++) {
+                        ItemStack compass = allCompii.get(i);
+                        CompassMeta newMeta = (CompassMeta)compass.getItemMeta();
+                        String itemName = newMeta.getDisplayName();
+                        if (itemName.contains("Tracker")) {
+                            String hunteeName = itemName.split(" ")[0];
+                            Player huntee = Bukkit.getPlayer(hunteeName);
+                            newMeta.setLodestoneTracked(false);
+                            newMeta.setLodestone(huntee.getLocation());
+                            compass.setItemMeta(newMeta);
+                        }
+                    }
                 }
-            }, 0L, 10L);
+            }, 0L, 20L);
         }
 
         return true;
