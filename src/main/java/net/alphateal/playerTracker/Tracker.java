22ac829111f1eb83d71ffc38b4b86af02669237b
Joey Gore <joey@joeygore.dev>
2020-12-16 20:25:19 -0500

    Removed unused imports

--- src/main/java/net/alphateal/playerTracker/Tracker.java
+++ src/main/java/net/alphateal/playerTracker/Tracker.java
@@ -3,21 +3,15 @@ package net.alphateal.playerTracker;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.Command;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.CompassMeta;
-import org.bukkit.material.MaterialData;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
 
 import java.util.HashMap;
-import java.util.Map;
-
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.data.BlockData;
 
 public class Tracker implements CommandExecutor {
 
